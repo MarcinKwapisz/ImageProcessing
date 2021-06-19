3c147b76084bd7b70342095bbe3fe05025daaf2a
Marcin <marcinos180@gmail.com>
2021-06-19 23:26:13 +0200

    04 added

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/marcin/PycharmProjects/SkeletonAppPO/venv"
+set -gx VIRTUAL_ENV "/home/marcin/Projekty/PycharmProjects/SkeletonAppPO/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
